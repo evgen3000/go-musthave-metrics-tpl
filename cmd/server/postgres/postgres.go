eb8a9f3d520c9a68798c533faf77c95a841d92af
Yakovishin Evgeny <00705@inbox.ru>
2024-11-02 16:53:35 +0300

    fix: postgres.go

--- cmd/server/postgres/postgres.go
+++ cmd/server/postgres/postgres.go
@@ -7,9 +7,7 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-type Container struct {
-	DBPool *pgxpool.Pool
-} //postgresql://admin:admin@localhost:5432/admin?schema=public
+//postgresql://admin:admin@localhost:5432/admin?schema=public
 
 func Connect(dsn string) *pgxpool.Pool {
 
@@ -23,16 +21,16 @@ func Connect(dsn string) *pgxpool.Pool {
 		log.Fatalf("Unable to connect to database: %v", err)
 	}
 
-	//q := `create table gauge
-	//		(id varchar(256) primary key,
-	//		value double precision);
-	//	create table counter (
-	//	    id varchar(256) primary key,
-	//	    value integer)`
-	//_, errExec := pool.Exec(context.Background(), q)
-	//if errExec != nil {
-	//	log.Fatalf("Unable to create table: %v", err)
-	//}
+	q := `create table gauge
+			(id varchar(256) primary key,
+			value double precision);
+		create table counter (
+		    id varchar(256) primary key,
+		    value integer)`
+	_, errExec := pool.Exec(context.Background(), q)
+	if errExec != nil {
+		log.Fatalf("Unable to create table: %v", err)
+	}
 	pingErr := pool.Ping(context.Background())
 	if pingErr != nil {
 		log.Fatalf("Unable to ping database: %v", pingErr)
