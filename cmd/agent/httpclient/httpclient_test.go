42afbbd92e11704a9fd1876ddf7ca33246560806
Yakovishin Evgeny <00705@inbox.ru>
2024-11-12 05:51:20 +0300

    feat: add tests for file client

---
+++ cmd/agent/httpclient/httpclient_test.go
@@ -0,0 +1,50 @@
+package httpclient_test
+
+import (
+	"bytes"
+	"compress/gzip"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"evgen3000/go-musthave-metrics-tpl.git/cmd/agent/httpclient"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHTTPClient_SendMetrics(t *testing.T) {
+	testHost := "localhost:8080"
+	client := httpclient.NewHTTPClient(testHost)
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "/updates/", r.URL.Path)
+		assert.Equal(t, http.MethodPost, r.Method)
+		assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
+		assert.Equal(t, "gzip", r.Header.Get("Content-Encoding"))
+
+		// Check that the body is correctly gzipped
+		gzipReader, err := gzip.NewReader(r.Body)
+		require.NoError(t, err)
+		defer func(gzipReader *gzip.Reader) {
+			err := gzipReader.Close()
+			if err != nil {
+
+			}
+		}(gzipReader)
+
+		var buf bytes.Buffer
+		_, err = buf.ReadFrom(gzipReader)
+		require.NoError(t, err)
+
+		expected := `[{"id":"testMetric","type":"gauge","value":42.42}]`
+		assert.JSONEq(t, expected, buf.String())
+
+		w.WriteHeader(http.StatusOK)
+	}))
+	defer server.Close()
+
+	client = httpclient.NewHTTPClient(server.Listener.Addr().String())
+
+	data := `[{"id":"testMetric","type":"gauge","value":42.42}]`
+	client.SendMetrics([]byte(data))
+}
