80934ebc2e05332d4d978048f09d2ef44dad48a7
Yakovishin Evgeny <00705@inbox.ru>
2024-11-18 00:39:19 +0300

    feat: add crypto key to config

--- internal/config/agent/agent.go
+++ internal/config/agent/agent.go
@@ -10,17 +10,20 @@ type Config struct {
 	PoolInterval   int
 	ReportInterval int
 	Host           string
+	CryptoKey      string
 }
 
 func GetAgentConfig() *Config {
 	reportIntervalFlag := flag.Int("r", 10, "Report interval in seconds.")
 	pollIntervalFlag := flag.Int("p", 2, "Pool interval in seconds.")
 	hostFlag := flag.String("a", "localhost:8080", "Host IP address and port.")
+	cryptoKey := flag.String("k", "", "AES encryption key.")
 	flag.Parse()
 
 	return &Config{
 		PoolInterval:   utils.GetIntValue("POLL_INTERVAL", *pollIntervalFlag),
 		ReportInterval: utils.GetIntValue("REPORT_INTERVAL", *reportIntervalFlag),
 		Host:           utils.GetStringValue("ADDRESS", *hostFlag),
+		CryptoKey:      *cryptoKey,
 	}
 }
